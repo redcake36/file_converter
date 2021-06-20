8d3123dccf9e51abab6bdb83aef5fc3cde6c9c77
redcake36 <kan.denis10@gmail.com>
2021-06-20 15:44:11 +0300

    Add project files.

---
+++ file_converter/Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.Json;
+using System.Xml;
+
+namespace file_converter
+{
+    public class Data
+    {
+        public List<string> fields;
+        public List<List<string>> values;
+
+
+        public void xml_parser(string file_name)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(file_name);
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+        }
+    }
+    class P
+    {
+        public string name { get; set; }
+        public string role { get; set; }
+        public string last_login { get; set; }
+    }
+    class Program
+    {
+        public static Data csv_parser(string file_name)
+        {
+            Data d = new Data();
+            List<string> f = new List<string>();
+            List<List<string>> v = new List<List<string>>();
+
+            string[] lines = System.IO.File.ReadAllLines(file_name);
+            int i = 0;
+            string word = "";
+            while (i < lines[0].Length)
+            {
+                while (lines[0][i] != ',' & i < lines[0].Length - 1)
+                {
+                    word += lines[0][i];
+                    i++;
+                }
+                if (i == lines[0].Length - 1)
+                {
+                    word += lines[0][i];
+                }
+                f.Add(word);
+                word = "";
+                i++;
+            }
+
+
+            for (int k = 1; k < lines.Length; k++)
+            {
+                List<string> a = new List<string>();
+                int u = 0;
+                i = 0;
+                word = "";
+                while (i < lines[k].Length)
+                {
+                    while (lines[k][i] != ',' & i < lines[k].Length - 1)
+                    {
+                        word += lines[k][i];
+                        i++;
+                    }
+                    if (i == lines[k].Length - 1)
+                    {
+                        word += lines[k][i];
+                    }
+                    a.Add(word);
+                    word = "";
+                    u++;
+                    i++;
+                }
+                v.Add(a);
+            }
+            d.fields = f;
+            d.values = v;
+            return (d);
+        }
+        public static Data txt_parser(string file_name)
+        {
+            Data d = new Data();
+            string[] lines = System.IO.File.ReadAllLines(file_name);
+            List<string> f = new List<string>();
+            List<List<string>> v = new List<List<string>>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                List<string> a = new List<string>();
+                if (lines[i].Contains("Object"))
+                {
+                    i++;
+                    while (!lines[i].Contains("End of object"))
+                    {
+                        string[] subs = lines[i].Split(": ");
+                        f.Add(subs[0]);
+                        a.Add(subs[1]);
+                        i++;
+                    }
+                }
+                v.Add(a);
+            }
+            d.fields = f.Distinct().ToList();
+            d.values = v;
+            return (d);
+        }
+        public static void txt_exporter(Data d, string file_name)
+        {
+            if (d.fields.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(file_name))
+                {
+                    for (int i = 0; i < d.values.Count; i++)
+                    {
+                        sw.WriteLine("=== Object ===");
+                        for (int j = 0; j < d.fields.Count; j++)
+                        {
+                            sw.WriteLine("{0}: {1}", d.fields[j], d.values[i][j]);
+                        }
+                        sw.WriteLine("=== End of object ===");
+                    }
+                }
+            }
+        }
+        public static void csv_exporter(Data d, string file_name)
+        {
+            if (d.fields.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(file_name))
+                {
+                    for (int j = 0; j < d.fields.Count; j++)
+                    {
+                        if (j == d.fields.Count - 1)
+                        {
+                            sw.Write("{0}\n", d.fields[j]);
+                        }
+                        else
+                        {
+                            sw.Write("{0},", d.fields[j]);
+                        }
+                    }
+                    for (int i = 0; i < d.values.Count; i++)
+                    {
+                        for (int j = 0; j < d.fields.Count; j++)
+                        {
+                            if (j == d.fields.Count - 1)
+                            {
+                                sw.Write("{0}\n", d.values[i][j]);
+                            }
+                            else
+                            {
+                                sw.Write("{0},", d.values[i][j]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static void xml_exporter(Data d, string file_name)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(file_name);
+            XmlElement xRoot = xDoc.DocumentElement;
+
+
+            for (int i = 0; i < d.values.Count; i++)
+            {
+                XmlElement ObjectElem = xDoc.CreateElement("Object");
+                for (int j = 0; j < d.fields.Count; j++)
+                {
+                    XmlElement PropertyElem = xDoc.CreateElement("Property");
+                    XmlAttribute typeAttr = xDoc.CreateAttribute("type");
+                    XmlAttribute nameAttr = xDoc.CreateAttribute("name");
+
+                    XmlText PropertyText = xDoc.CreateTextNode(d.values[i][j]);
+                    XmlText typeText = xDoc.CreateTextNode("string");
+                    XmlText nameText = xDoc.CreateTextNode(d.fields[j]);
+
+                    PropertyElem.AppendChild(PropertyText);
+                    typeAttr.AppendChild(typeText);
+                    nameAttr.AppendChild(nameText);
+
+                    PropertyElem.Attributes.Append(typeAttr);
+                    PropertyElem.Attributes.Append(nameAttr);
+                    ObjectElem.AppendChild(PropertyElem);
+                }
+                xRoot.AppendChild(ObjectElem);
+            }
+
+            xDoc.Save(file_name);
+        }
+        public static void json_exporter(Data d, string file_name)
+        {
+            if (d.fields.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(file_name))
+                {
+                    sw.WriteLine("[");
+                    for (int i = 0; i < d.values.Count; i++)
+                    {
+                        sw.WriteLine("\t{");
+                        for (int j = 0; j < d.fields.Count; j++)
+                        {
+                            sw.Write("\t\t\"{0}\": \"{1}\"", d.fields[j], d.values[i][j]);
+                            if (j == d.fields.Count - 1)
+                                sw.Write("\n");
+                            else
+                                sw.Write(",\n");
+                        }
+
+                        if (i == d.values.Count - 1)
+                            sw.WriteLine("\t}");
+                        else
+                            sw.WriteLine("\t},");
+
+                    }
+                    sw.Write("]");
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Data mydata = new Data();
+            //mydata = csv_parser(args[1]);
+            //txt_exporter(mydata,args[0]);
+            string[] frmt = { "txt", "csv", "json", "xml" };
+            string[] arg_z = args[0].Split(".");
+            string[] arg_o = args[1].Split(".");
+            if (frmt.Contains(arg_z[arg_z.Length - 1]) & frmt.Contains(arg_o[arg_o.Length - 1]))
+            {
+                mydata = txt_parser(args[0]);
+                json_exporter(mydata, args[1]);
+
+                foreach (List<string> v in mydata.values)
+                {
+                    foreach (string s in v)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+
+                //mydata.txt_parser(args[0]);
+
+                //for (int i = 0; i < mydata.fields.Count; i++)
+                //{
+                //    Console.WriteLine(mydata.fields[i]);
+                //}
+
+
+
+                //Console.WriteLine("Введите строку для записи в файл:");
+                //string text = Console.ReadLine();
+                //File.WriteAllText(args[0], text);
+
+
+
+                //string[] lines = System.IO.File.ReadAllLines(args[1]);
+                //foreach (string line in lines)
+                //{
+                //    Console.WriteLine(line);
+                //}
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Can`t convert such type of file :C");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ file_converter/type.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace file_converter
+{
+    public interface abstractFile
+    {
+        public string file_name { get; set; }
+        public Data parser();
+        public void exporter(Data d, string file_name);
+    }
+    public class file_txt : abstractFile
+    {
+        public string file_name { get; set; }
+        public Data parser()
+        {
+            return new Data();
+        }
+        public void exporter(Data d, string file_name)
+        {
+
+        }
+    }
+    public class file_json : abstractFile
+    {
+        public string file_name { get; set; }
+        public Data parser()
+        {
+            return new Data();
+        }
+        public void exporter(Data d, string file_name)
+        {
+
+        }
+    }
+    public class file_csv : abstractFile
+    {
+        public string file_name { get; set; }
+        public Data parser()
+        {
+            return new Data();
+        }
+        public void exporter(Data d, string file_name)
+        {
+
+        }
+    }
+    public class file_xml : abstractFile
+    {
+        public string file_name { get; set; }
+        public Data parser()
+        {
+            return new Data();
+        }
+        public void exporter(Data d, string file_name)
+        {
+
+        }
+    }
+}
